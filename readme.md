1cb10e048b7daa608efa6d7698e7c89205755108
Rodrigo Coutinho <rodrigo.coutinho@titaniumfix.corp>
2024-07-23 14:44:06 -0300

    aula1

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Node.js
+in file 'package.json' add in 'scripts' for run server.js automatic: 
+    "dev": "node --watch src/server.js"
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
+import { addAbortSignal } from 'node:stream'
 
 const server = http.createServer((req, res) => {
-    return res.end('hello world1')
+    return res.end('hello 3')
 })
 
 server.listen(3333) //localhost:3333
